736174125fa6402d668ee1c15ec65b39eea35af4
rmaher84 <rmaher84@gmail.com>
2021-08-08 18:38:36 +0200

    initial changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
+import { Document, Page } from 'react-pdf/dist/umd/entry.parcel';
+import Menu from './assets/menu.pdf';
 
-const App = () => (
-    <div>mexi-pizza website</div>
-);
+const App = () => {
+    const [numPages, setNumPages] = React.useState(null);
+    const [pageNumber, setPageNumber] = React.useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
+
+    return (
+        <div>
+            <Document
+                file={Menu}
+                onLoadSuccess={onDocumentLoadSuccess}
+            >
+                <Page pageNumber={2} />
+                <Page pageNumber={pageNumber} />
+            </Document>
+            {/* <p>Page {pageNumber} of {numPages}</p> */}
+        </div>
+    );
+}
+
 
 ReactDOM.render(<App />, document.getElementById('app'))
