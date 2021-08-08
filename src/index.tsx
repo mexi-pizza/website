281f1daf67c03f7227cd7044e78aace18b6eb4e5
rmaher84 <rmaher84@gmail.com>
2021-08-08 18:01:29 +0200

    initial changes

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>mexi-pizza website</div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'))
